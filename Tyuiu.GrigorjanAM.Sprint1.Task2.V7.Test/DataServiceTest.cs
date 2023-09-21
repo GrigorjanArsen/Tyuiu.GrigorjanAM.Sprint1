6e127e1b9b73ab77036a3c36bfa495bc66e06cd5
Tyuiu.GrigorjanAM@outlook.com <djuravlik323@gmail.com>
2023-09-21 21:30:35 +0500

    Task2

---
+++ Tyuiu.GrigorjanAM.Sprint1.Task2.V7.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GrigorjanAM.Sprint1.Task2.V7.Lib;
+namespace Tyuiu.GrigorjanAM.Sprint1.Task2.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            var res = ds.CalculateSquareCircle(x);
+            Assert.AreEqual(12.56, res);
+        }
+    }
+}
---
+++ Tyuiu.GrigorjanAM.Sprint1.Task2.V7/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GrigorjanAM.Sprint1.Task2.V7.Lib;
+namespace Tyuiu.GrigorjanAM.Sprint1.Task2.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Григорян А. М. | ИИПБ-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Григорян Арсен Мовсесович | ИИПБ-23-3                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет площадь круга по формуле 3.14 * R^2 и печатает её на экране.  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int x;
+            Console.WriteLine("Введите значение радиуса круга:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Примерная площадь круга с заданным радиусом равна: " + ds.CalculateSquareCircle(x));
+            
+            Console.ReadKey();
+        }
+    }
+}
